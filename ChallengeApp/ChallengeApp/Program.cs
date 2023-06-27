de4a3a58be921de5fa996e9c6d7a961e0e9de878
Zuza K <zuzanna.kaminska@o2.pl>
2023-06-27 18:43:19 +0200

    Zadanie z dnia 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.ComponentModel.Design;
+
+var name = "Ewa";
+var age = 33;
+string sex = "Kobieta";
+if (age < 18)
+{
+    if (sex == "Kobieta")
+    {
+        Console.WriteLine("Niepełnoletnia kobieta");
+    }
+    else
+    {
+        Console.WriteLine("Niepełnoletni mężczyzna");
+    }
+}
+else if (age < 30)
+{
+    if (sex == "Kobieta")
+    {
+        Console.WriteLine("Kobieta poniżej 30 lat");
+    }
+    else
+    {
+        Console.WriteLine("Mężczyzna poniżej 30 lat");
+    }
+}
+else if (age >= 30 && name != "Ewa")
+{
+    if (sex == "Kobieta")
+    {
+        Console.WriteLine("Kobieta 30 lat i więcej");
+    }
+    else
+    {
+        Console.WriteLine("Mężczyzna 30 lat i więcej");
+    }
+}
+else if (name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
