b3395923395972858dde147fa8a356367a00d4fa
Zuza K <zuzanna.kaminska@o2.pl>
2023-06-28 19:16:57 +0200

    Zadanie dzień 5

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,43 +1,24 @@
-﻿using System;
-using System.ComponentModel.Design;
+﻿int number = 658455940;
+string numberInString = number.ToString();
+char[] numbers = numberInString.ToCharArray();
+Console.WriteLine("Moja liczba to:" + number);
+Console.WriteLine("Ilość poszczególnych cyfr, kóre zawiera moja liczba:");
+int[] counter = new int[10];
 
-var name = "Ewa";
-var age = 33;
-string sex = "Kobieta";
-if (age < 18)
+foreach (char digit in numbers)
+{ 
+    if (digit == '0') counter[0]++;
+    else if (digit == '1') counter[1]++;
+    else if (digit == '2') counter[2]++;
+    else if (digit == '3') counter[3]++;
+    else if (digit == '4') counter[4]++;
+    else if (digit == '5') counter[5]++;
+    else if (digit == '6') counter[6]++;
+    else if (digit == '7') counter[7]++;
+    else if (digit == '8') counter[8]++;
+    else if (digit =='9') counter[9]++;
+    }
+for (int i = 0; i < numbers.Length; i++)
 {
-    if (sex == "Kobieta")
-    {
-        Console.WriteLine("Niepełnoletnia kobieta");
-    }
-    else
-    {
-        Console.WriteLine("Niepełnoletni mężczyzna");
-    }
-}
-else if (age < 30)
-{
-    if (sex == "Kobieta")
-    {
-        Console.WriteLine("Kobieta poniżej 30 lat");
-    }
-    else
-    {
-        Console.WriteLine("Mężczyzna poniżej 30 lat");
-    }
-}
-else if (age >= 30 && name != "Ewa")
-{
-    if (sex == "Kobieta")
-    {
-        Console.WriteLine("Kobieta 30 lat i więcej");
-    }
-    else
-    {
-        Console.WriteLine("Mężczyzna 30 lat i więcej");
-    }
-}
-else if (name == "Ewa" && age == 33)
-{
-    Console.WriteLine("Ewa, lat 33");
+    Console.WriteLine( i + "->" + counter[i]);
 }
