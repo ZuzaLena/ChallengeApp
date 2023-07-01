ff80cba13ac6ea2db5c4d16ca0f330935790b32c
Zuza K <zuzanna.kaminska@o2.pl>
2023-07-02 00:15:53 +0200

    Zadanie z dnia 6

---
+++ ChallengeApp/ChallengeApp/Employee.cs
@@ -0,0 +1,40 @@
+﻿
+namespace ChallengeApp
+{
+    public class Employee
+    {
+        private List<int> score = new List<int>();
+
+            public Employee(string name, string surname, int age, int score)
+        {
+            this.Name = name;
+            this.Surname = surname;
+            this.Age = age;
+            this.Score = 0;
+        }
+       
+
+        public string Name { get; private set; }
+        public string Surname { get; private set; }
+        public int Age { get; private set; }
+        public int Score { get; private set; }
+       
+        public int Result
+        {
+          get
+          {
+               return this.score.Sum();
+
+          }
+        }
+
+        public void AddScore(int number)
+        {
+            this.score.Add(number);
+        }
+        
+
+    }
+}
+
+ 
--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,24 +1,39 @@
-﻿int number = 658455940;
-string numberInString = number.ToString();
-char[] numbers = numberInString.ToCharArray();
-Console.WriteLine("Moja liczba to:" + number);
-Console.WriteLine("Ilość poszczególnych cyfr, kóre zawiera moja liczba:");
-int[] counter = new int[10];
+﻿using ChallengeApp;
 
-foreach (char digit in numbers)
-{ 
-    if (digit == '0') counter[0]++;
-    else if (digit == '1') counter[1]++;
-    else if (digit == '2') counter[2]++;
-    else if (digit == '3') counter[3]++;
-    else if (digit == '4') counter[4]++;
-    else if (digit == '5') counter[5]++;
-    else if (digit == '6') counter[6]++;
-    else if (digit == '7') counter[7]++;
-    else if (digit == '8') counter[8]++;
-    else if (digit =='9') counter[9]++;
-    }
-for (int i = 0; i < numbers.Length; i++)
+Employee user1 = new Employee("Adam", "Nowak", 32, 0);
+Employee user2 = new Employee("Zuza", "Kamińska", 35, 0);
+Employee user3 = new Employee("Lena", "Kowalska", 29, 0);
+
+List<Employee> users = new List<Employee>()
 {
-    Console.WriteLine( i + "->" + counter[i]);
+   user1, user2, user3
+};
+
+int maxResult = -1;
+Employee userWithMaxResult = null;
+
+foreach (var user in users)
+{
+    if (user.Result > maxResult)
+    {
+        userWithMaxResult = user;
+        maxResult = user.Result;
+    }
 }
+
+user1.AddScore(5);
+user1.AddScore(6);
+user1.AddScore(8);
+
+user2.AddScore(6);
+user2.AddScore(7);
+user2.AddScore(3);
+
+user3.AddScore(2);
+user3.AddScore(9);
+user3.AddScore(4);
+
+Console.WriteLine(userWithMaxResult.Name + " " + userWithMaxResult.Surname + " lat " + userWithMaxResult.Age +" ilość punktów " + userWithMaxResult.Result);
+
+
+
